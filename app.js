a4475a05f1bffbafef147e67ef28d746c8884045
Lester Caletina <caletina.marklester@gmail.com>
2022-03-04 18:04:18 +0800

    add start

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connectDb = require('./db/connect')
 ///Require DOTENV
 
 require('dotenv').config()
-MiddleWare
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Origin', '*')
